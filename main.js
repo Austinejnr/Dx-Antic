032f4f91157563a7e1d08105af9b47db199cbb7c
Austinejnr <samuelamoto024@gmail.com>
2022-10-19 11:53:55 -0800

    almost ready

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ var hamburger = document.querySelector(".hamburger-container")
 
 var mobilelinkscontainer = document.querySelector(".mobilelinkscontainer")
 console.log(hamburger)
+const menu_btn = document.querySelector(".mobilelinkscontainer");
 
 
 
 function addandremove(){
     hamburger.classList.toggle('showburger')
     mobilelinkscontainer.classList.toggle('showmobilelinks') 
+    menu_btn.classList.toggle("is-active");
 }
+   
+
